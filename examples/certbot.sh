84a03b2a921ad0ece26a59277be62dc330516e8f
Craig Willis <willis8@illinois.edu>
2016-05-19 12:55:15 -0500

    Added examples from master

--- examples/certbot.sh
+++
@@ -1,6 +0,0 @@
-docker run -it \
-           --rm \
-           --net host \
-            -v /etc/letsencrypt:/etc/letsencrypt \
-            -v /var/lib/letsencrypt:/var/lib/letsencrypt \
-            gzm55/certbot certonly --manual
